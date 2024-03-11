25d07fdb681026c4b4578ab5d182036eed792063
PawelBybel <161841229+PawelBybel@users.noreply.github.com>
2024-03-11 11:50:14 +0100

    Delete src directory

--- src/main/java/com/example/Quiz/Answers/AnswersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Quiz.Answers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswersRepository
-        extends JpaRepository<Answers,Integer>
-{
-
-}
--- src/main/java/com/example/Quiz/Questions/AddQuestions.java
+++
@@ -1,6 +0,0 @@
-package com.example.Quiz.Questions;
-
-public record AddQuestions(String name) {
-
-    }
-
