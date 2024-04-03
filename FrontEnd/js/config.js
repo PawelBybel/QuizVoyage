a61626c42c2eb56aa30dde953acf92be39015d85
PawelBybel <161841229+PawelBybel@users.noreply.github.com>
2024-04-03 12:39:02 +0200

    Delete FrontEnd directory

--- FrontEnd/js/config.js
+++
@@ -1,9 +0,0 @@
-export const API_URL = 'http://localhost:8080/api/v1';
-export const API_URL_ALL = '/quiz/id/all';
-let randomDecimal = Math.random();
-// export let quizNumber = Math.floor(randomDecimal * 2) + 1;
-export const quizNumber = 1;
-export const questionNumber = 0;
-export const TIMEOUT_SECONDS = 10;
-
-
